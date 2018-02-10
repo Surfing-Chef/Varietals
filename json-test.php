4632dd4e1e7f6f3dc22687babc170cfd9c6a6966
Surfing-Chef <surfing-chef@surfing-chef.com>
2018-02-10 14:47:09 -0800

    Adjusted path to css files

--- json-test.php
+++ json-test.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="jquery-ui.css">
-    <link rel="stylesheet" href="jquery-ui.structure.css">
-    <link rel="stylesheet" href="jquery-ui.theme.css">
+    <link rel="stylesheet" href="jquery-ui/jquery-ui.css">
+    <link rel="stylesheet" href="jquery-ui/jquery-ui.structure.css">
+    <link rel="stylesheet" href="jquery-ui/jquery-ui.theme.css">
 
     <title>JSON Matches Food and Wine</title>
 </head>
