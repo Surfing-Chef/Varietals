5fa0899ac2500a7e4ed200374e10eca74b9a4077
Surfing-Chef <surfing-chef@surfing-chef.com>
2018-02-11 14:03:29 -0800

    Added error handler to display/bypass error for correction

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+function handleError (error) {
+    console.log(error.toString())
+    this.emit('end')
+}
+
 gulp.task('sass', function(){
     return gulp.src('app/scss/**/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
