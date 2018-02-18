526ef8bb2c4559b7f906918edcfecb579f98007f
Surfing-Chef <surfing-chef@surfing-chef.com>
2018-02-17 23:13:43 -0800

    Created structure of varietal accordion

--- app/index.php
+++ app/index.php
@@ -10,8 +10,26 @@
     <title>JSON Matches Food and Wine 1.0</title>
 </head>
 <body>
-
-
+<ul class="accordion">
+  <li>
+    <a class="toggle" href="javascript:void(0);">Category</a>
+    <ul class="inner">
+      <li>
+        <a href="#" class="toggle">Subcategory</a>
+        <ul class="inner">
+          <li>
+            <a href="#" class="toggle">Prep</a>
+            <ul class="inner">
+                <li>Varietal 1</li>
+                <li>Varietal 2</li>
+                <li>Varietal 3</li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+</ul>
 
 <script
   src="http://code.jquery.com/jquery-3.3.1.min.js"
